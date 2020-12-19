e9e9b6021cf875f8f4f22748352dd38b10219a8b
K4fle D. Manish <k4fledmanish@aol.com>
2020-12-19 22:07:33 +1100

    Saved the state of message in TextInput

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React,  { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,  { useEffect , useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 //import { useEffect } from 'react';
 //let io = require('socket.io-client/socket.io');
 
@@ -13,13 +13,21 @@ var io = require("socket.io-client/socket.io");
 
 export default function App() {
 
-  useEffect(function(){
+  // Using useState to save the user input
+    const [messageToSend, setMessageToSend] = useState("hjhjhjhj");
+
+
+  useEffect(() => {
     io("http://10.0.0.1:3000")
   },[])
 
   return (
     <View style={styles.container}>
       <Text>This is React Native ! Test 8</Text>
+      <TextInput 
+      value = {messageToSend} 
+      onChangeText={text => setMessageToSend(test)} 
+      placeholder="Enter chat message.." />
       <StatusBar style="auto" />
     </View>
   );
