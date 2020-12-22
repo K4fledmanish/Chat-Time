33d92834f15dc693edaf0c9493c42f409b6764de
K4fle D. Manish <k4fledmanish@aol.com>
2020-12-22 11:47:54 +1100

    Added FriendList Screen

--- screens/AppContainer.js
+++ screens/AppContainer.js
@@ -1,9 +1,10 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import FriendListScreen from './FriendListScreen';
 import HomeScreen from "./HomeScreen";
 import JoinScreen from "./JoinScreen";
 
-const AppStack = createStackNavigator({ Home: HomeScreen });
+const AppStack = createStackNavigator({ Home: FriendListScreen, Chat: HomeScreen });
 
 export default createAppContainer(
     createSwitchNavigator(
