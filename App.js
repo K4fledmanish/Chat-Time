fbed0260a9586e74c6569233cfd4e38b71f00aff
K4fle D. Manish <k4fledmanish@aol.com>
2020-12-22 12:27:22 +1100

    Finishing up Private Chat Feature

--- App.js
+++ App.js
@@ -8,16 +8,44 @@ import { Provider } from "react-redux";
 import createSocketIoMiddleware from "redux-socket.io";
 import io from "socket.io-client";
 import React from "react";
-import AppContainer from "./screens/AppContainer";
+import AppContainer from "./AppContainer";
+
+
 const socket = io("http://10.0.0.1:3001");
 const socketIoMiddleware = createSocketIoMiddleware(socket, "server/");
 
-function reducer(state = {}, action) {
+function reducer(state = { conversations: {} }, action) {
   switch (action.type) {
-    case "message":
-      return { ...state, message: action.data };
     case "users_online":
-      return { ...state, usersOnline: action.data };
+      const conversations = { ...state.conversations };
+      const usersOnline = action.data;
+      for (let i = 0; i < usersOnline.length; i++) {
+        const userId = usersOnline[i].userId;
+        if (conversations[userId] === undefined) {
+          conversations[userId] = {
+            messages: [],
+            username: usersOnline[i].username
+          };
+        }
+      }
+      return { ...state, usersOnline, conversations };
+    case "private_message":
+      const conversationId = action.data.conversationId;
+      return {
+        ...state,
+        conversations: {
+          ...state.conversations,
+          [conversationId]: {
+            ...state.conversations[conversationId],
+            messages: [
+              action.data.message,
+              ...state.conversations[conversationId].messages
+            ]
+          }
+        }
+      };
+    case "self_user":
+      return { ...state, selfUser: action.data };
     default:
       return state;
   }
@@ -28,7 +56,6 @@ const store = applyMiddleware(socketIoMiddleware)(createStore)(reducer);
 store.subscribe(() => {
   console.log("new state", store.getState());
 });
-store.dispatch({ type: "server/hello", data: "Hello!" });
 
 export default function App() {
   return (
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -20,21 +20,12 @@ io.on("connection", socket => {
   console.log("a user connected!");
   console.log(socket.id);
   users[socket.id] = { userId: uuidv1() };
-  socket.on("join", username => {
-    users[socket.id].username = username;
-    users[socket.id].avatar = createUserAvatarUrl();
-    messageHandler.handleMessage(socket, users);
-  });
   socket.on("disconnect", () => {
     delete users[socket.id];
     io.emit("action", { type: "users_online", data: createUsersOnline() });
   });
   socket.on("action", action => {
     switch (action.type) {
-      case "server/hello":
-        console.log("Got hello event", action.data);
-        socket.emit("action", { type: "message", data: "Good day!" });
-        break;
       case "server/join":
         console.log("Got join event", action.data);
         users[socket.id].username = action.data;
@@ -43,9 +34,27 @@ io.on("connection", socket => {
           type: "users_online",
           data: createUsersOnline()
         });
+        socket.emit("action", { type: "self_user", data: users[socket.id] });
         break;
-      case "server/private-message":
-        console.log("Got a private-message", action.data);
+      case "server/private_message":
+        const conversationId = action.data.conversationId;
+        const from = users[socket.id].userId;
+        const userValues = Object.values(users);
+        const socketIds = Object.keys(users);
+        for (let i = 0; i < userValues.length; i++) {
+          if (userValues[i].userId === conversationId) {
+            const socketId = socketIds[i];
+            io.sockets.sockets[socketId].emit("action", {
+              type: "private_message",
+              data: {
+                ...action.data,
+                conversationId: from
+              }
+            });
+            break;
+          }
+        }
+        break;
     }
   });
 });
